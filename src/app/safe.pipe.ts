31701efaf69760809acbc833742a74e07df42fb9
Shubham-ly <shubham.m@ly.design>
2022-08-25 11:12:41 +0530

    Added resource filter in SafePipe

--- src/app/safe.pipe.ts
+++ src/app/safe.pipe.ts
@@ -7,7 +7,10 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 export class SafePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(url: string): SafeHtml {
+  transform(url: string, type: string = ''): any {
+    if (type === 'resource')
+      return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    1;
     return this.sanitizer.bypassSecurityTrustHtml(url);
   }
 }
